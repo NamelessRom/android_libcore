multi-os-engine/libcore

Update our MIME type map.
Bug: https://code.google.com/p/android/issues/detail?id=61996
Change-Id: I55efc9d38c87d89bb20073e4632ab1e06c77be09

luni/src/main/java/libcore/net/MimeUtils.java
@@ -57,6 +57,7 @@ public final class MimeUtils {
         add("application/msaccess", "mdb");
         add("application/oda", "oda");
         add("application/ogg", "ogg");
+        add("application/ogg", "oga");
         add("application/pdf", "pdf");
         add("application/pgp-keys", "key");
         add("application/pgp-signature", "pgp");
@@ -209,11 +210,17 @@ public final class MimeUtils {
         add("application/x-xfig", "fig");
         add("application/xhtml+xml", "xhtml");
         add("audio/3gpp", "3gpp");
+        add("audio/aac", "aac");
+        add("audio/aac-adts", "aac");
         add("audio/amr", "amr");
+        add("audio/amr-wb", "awb");
         add("audio/basic", "snd");
+        add("audio/imelody", "imy");
         add("audio/midi", "mid");
         add("audio/midi", "midi");
+        add("audio/midi", "ota");
         add("audio/midi", "kar");
+        add("audio/midi", "rtttl");
         add("audio/midi", "xmf");
         add("audio/mobile-xmf", "mxmf");
         // add ".mp3" first so it will be the default for guessExtensionFromMimeType
@@ -228,6 +235,7 @@ public final class MimeUtils {
         add("audio/x-aiff", "aiff");
         add("audio/x-aiff", "aifc");
         add("audio/x-gsm", "gsm");
+        add("audio/x-matroska", "mka");
         add("audio/x-mpegurl", "m3u");
         add("audio/x-ms-wma", "wma");
         add("audio/x-ms-wax", "wax");
@@ -255,6 +263,7 @@ public final class MimeUtils {
         add("image/vnd.djvu", "djvu");
         add("image/vnd.djvu", "djv");
         add("image/vnd.wap.wbmp", "wbmp");
+        add("image/webp", "webp");
         add("image/x-cmu-raster", "ras");
         add("image/x-coreldraw", "cdr");
         add("image/x-coreldrawpattern", "pat");
@@ -331,12 +340,15 @@ public final class MimeUtils {
         add("text/x-vcard", "vcf");
         add("video/3gpp", "3gpp");
         add("video/3gpp", "3gp");
-        add("video/3gpp", "3g2");
+        add("video/3gpp2", "3gpp2");
+        add("video/3gpp2", "3g2");
+        add("video/avi", "avi");
         add("video/dl", "dl");
         add("video/dv", "dif");
         add("video/dv", "dv");
         add("video/fli", "fli");
         add("video/m4v", "m4v");
+        add("video/mp2ts", "ts");
         add("video/mpeg", "mpeg");
         add("video/mpeg", "mpg");
         add("video/mpeg", "mpe");
@@ -345,8 +357,10 @@ public final class MimeUtils {
         add("video/quicktime", "qt");
         add("video/quicktime", "mov");
         add("video/vnd.mpegurl", "mxu");
+        add("video/webm", "webm");
         add("video/x-la-asf", "lsf");
         add("video/x-la-asf", "lsx");
+        add("audio/x-matroska", "mkv");
         add("video/x-mng", "mng");
         add("video/x-ms-asf", "asf");
         add("video/x-ms-asf", "asx");
@@ -354,7 +368,6 @@ public final class MimeUtils {
         add("video/x-ms-wmv", "wmv");
         add("video/x-ms-wmx", "wmx");
         add("video/x-ms-wvx", "wvx");
-        add("video/x-msvideo", "avi");
         add("video/x-sgi-movie", "movie");
         add("video/x-webex", "wrf");
         add("x-conference/x-cooltalk", "ice");

NamelessRom/android_libcore

luni/src/main/java/libcore/net/MimeUtils.java
@@ -57,6 +57,7 @@ public final class MimeUtils {
         add("application/msaccess", "mdb");
         add("application/oda", "oda");
         add("application/ogg", "ogg");
+        add("application/ogg", "oga");
         add("application/pdf", "pdf");
         add("application/pgp-keys", "key");
         add("application/pgp-signature", "pgp");
@@ -212,11 +213,17 @@ public final class MimeUtils {
         add("application/x-xfig", "fig");
         add("application/xhtml+xml", "xhtml");
         add("audio/3gpp", "3gpp");
+        add("audio/aac", "aac");
+        add("audio/aac-adts", "aac");
         add("audio/amr", "amr");
+        add("audio/amr-wb", "awb");
         add("audio/basic", "snd");
+        add("audio/imelody", "imy");
         add("audio/midi", "mid");
         add("audio/midi", "midi");
+        add("audio/midi", "ota");
         add("audio/midi", "kar");
+        add("audio/midi", "rtttl");
         add("audio/midi", "xmf");
         add("audio/mobile-xmf", "mxmf");
         // add ".mp3" first so it will be the default for guessExtensionFromMimeType
@@ -231,6 +238,7 @@ public final class MimeUtils {
         add("audio/x-aiff", "aiff");
         add("audio/x-aiff", "aifc");
         add("audio/x-gsm", "gsm");
+        add("audio/x-matroska", "mka");
         add("audio/x-mpegurl", "m3u");
         add("audio/x-ms-wma", "wma");
         add("audio/x-ms-wax", "wax");
@@ -260,6 +268,7 @@ public final class MimeUtils {
         add("image/vnd.djvu", "djvu");
         add("image/vnd.djvu", "djv");
         add("image/vnd.wap.wbmp", "wbmp");
+        add("image/webp", "webp");
         add("image/x-cmu-raster", "ras");
         add("image/x-coreldraw", "cdr");
         add("image/x-coreldrawpattern", "pat");
@@ -336,12 +345,15 @@ public final class MimeUtils {
         add("text/x-vcard", "vcf");
         add("video/3gpp", "3gpp");
         add("video/3gpp", "3gp");
-        add("video/3gpp", "3g2");
+        add("video/3gpp2", "3gpp2");
+        add("video/3gpp2", "3g2");
+        add("video/avi", "avi");
         add("video/dl", "dl");
         add("video/dv", "dif");
         add("video/dv", "dv");
         add("video/fli", "fli");
         add("video/m4v", "m4v");
+        add("video/mp2ts", "ts");
         add("video/mpeg", "mpeg");
         add("video/mpeg", "mpg");
         add("video/mpeg", "mpe");
@@ -350,8 +362,10 @@ public final class MimeUtils {
         add("video/quicktime", "qt");
         add("video/quicktime", "mov");
         add("video/vnd.mpegurl", "mxu");
+        add("video/webm", "webm");
         add("video/x-la-asf", "lsf");
         add("video/x-la-asf", "lsx");
+        add("video/x-matroska", "mkv");
         add("video/x-mng", "mng");
         add("video/x-ms-asf", "asf");
         add("video/x-ms-asf", "asx");
@@ -359,7 +373,6 @@ public final class MimeUtils {
         add("video/x-ms-wmv", "wmv");
         add("video/x-ms-wmx", "wmx");
         add("video/x-ms-wvx", "wvx");
-        add("video/x-msvideo", "avi");
         add("video/x-sgi-movie", "movie");
         add("video/x-webex", "wrf");
         add("x-conference/x-cooltalk", "ice");